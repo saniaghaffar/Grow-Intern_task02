1f1868e9e1024504b1ef86710a0aa26856a6cdbd
saniaghaffar <saniaghaffar55@gmail.com>
2024-08-31 12:01:52 +0500

    first commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let movies=[
+          {
+                    name:'Jhansi',
+                    des:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                    image:'images/slider-1.png'
+          },
+
+          {
+                    name:'The Warrior',
+                    des:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                    image:'images/slider-2.png'
+          },
+
+          {
+                    name:'Exposed',
+                    des:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                    image:'images/slider-3.png'
+          },
+
+          {
+                    name:'Vikram Vedda',
+                    des:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                    image:'images/slider-4.png'
+          },
+
+          {
+                    name:'Bheemlanayak',
+                    des:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+                    image:'images/slider-5.png'
+          }
+
+]
